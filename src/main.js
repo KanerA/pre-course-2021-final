0a8c377c3e34adac41a4a0b678014988835de76b
KanerA <kaner.assaf@gmail.com>
2021-01-27 17:57:45 +0200

    add check for empty input
    the event listener checks for an empty string before creating the divs, so it won't create them for nothing.

--- src/main.js
+++ src/main.js
@@ -9,31 +9,37 @@ addBtn.addEventListener('click', addToDo);
 
 function addToDo(event){
     event.preventDefault();
+    // ----- append the input text to text div ---- //
+    let inputElem = document.getElementById('text-input');
+    let textInput = inputElem.value;
+    if(textInput === ""){
+        alert('must add to-do');
+        return false;
+    }
+    
+    // ----- clear input field and focus for next input --- //
+    inputElem.value = "";
+    inputElem.focus();
+    
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo-container');
-
+    
     const todoText = document.createElement('div');
     todoText.classList.add('todo-text');
-
+    
     const createdAt = document.createElement('div');
     createdAt.classList.add('todo-createdAt');
-
+    
     const priority = document.createElement('div');
     priority.classList.add('todo-priority');
-
+    
     todoDiv.appendChild(priority);
     todoDiv.appendChild(createdAt);
     todoDiv.appendChild(todoText);
     
     viewSection.appendChild(todoDiv);
-
-    // ----- append the input text to text div ---- //
-    let inputElem = document.getElementById('text-input');
-    let textInput = inputElem.value;
-    todoText.innerHTML = textInput;
     
-    // ----- clear input field and focus for next input --- //
-    inputElem.value = "";
-    inputElem.focus();
+    // ----- adding data to each of the divs ----- //
+    todoText.innerHTML = textInput;
 
 }
