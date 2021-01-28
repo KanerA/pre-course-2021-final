15c5b30157ab2314f7d26d836b592f464e05f866
KanerA <kaner.assaf@gmail.com>
2021-01-28 22:09:43 +0200

    add get & put functions
    also taking the sorting div insert functionality to its own function (line 91).

--- src/main.js
+++ src/main.js
@@ -15,6 +15,13 @@ class toDoTask {
 
 addBtn.addEventListener('click', addToDo);
 sortBtn.addEventListener('click', sortToDo);
+document.addEventListener('DOMContentLoaded',async () => {
+    let myContent = await getTodoJson();
+    console.log(myContent);
+    for(let i = 0; i < myContent.length; i++){
+        createListItem(i);
+    }
+});
 
 // ---------------- Functions --------------------------- //
 
@@ -65,35 +72,56 @@ function addToDo(event){
     // ----- clear input field and focus for next input --- //
     inputElem.value = "";
     inputElem.focus();
-}
 
+    myTodo = {'myTodo': todoList};
+    updateTodoJson();
+}
 
 function sortToDo(){
     todoList.sort(function(a, b){
         return b.priority - a.priority;
     });
-
+    
     viewSection.innerHTML = "";
     for(let i=0; i < todoList.length; i++){
+        createListItem(i);
+    }
+}
+
+function createListItem(index){
+    if(todoList === []){
+
         const todoDiv = document.createElement('div');
         todoDiv.classList.add('todo-container');
-    
+        console.log(todoList);
         priorityDiv = document.createElement('div');
         priorityDiv.classList.add('todo-priority');
-        priorityDiv.append(todoList[i].priority);
-
+        priorityDiv.append(todoList[index].priority);
+        
         createdAtDiv = document.createElement('div');
         createdAtDiv.classList.add('todo-created-at');
-        createdAtDiv.append(todoList[i].date);
-
+        createdAtDiv.append(todoList[index].date);
+        
         textDiv = document.createElement('div');
         textDiv.classList.add('todo-text');
-        textDiv.append(todoList[i].text);
-
+        textDiv.append(todoList[index].text);
+        
         todoDiv.append(priorityDiv);
         todoDiv.append(createdAtDiv);
         todoDiv.append(textDiv);
         
         viewSection.append(todoDiv);
     }
+}
+
+async function updateTodoJson(){
+    await fetch("https://api.jsonbin.io/v3/b/6012eb097bfaff74c39959f7",{method:'put',headers: {'content-type': 'application/json'},body: JSON.stringify(myTodo)});
+}
+
+async function getTodoJson(){
+    let response = await fetch('https://api.jsonbin.io/v3/b/6012eb097bfaff74c39959f7/latest');
+    let jsonResponse = await response.json(); 
+    let recordResponse = jsonResponse["record"];
+    let myList = recordResponse; 
+    return myList.myTodo;      
 }
